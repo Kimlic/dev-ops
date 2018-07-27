902ce30d84acc327671a37b6d5bd387a258df2cf
alex <alex@pharosproduction.com>
2018-07-27 13:55:19 +0300

    added tomcat service

--- configs/phoenix_debian.sh
+++ configs/phoenix_debian.sh
@@ -8,11 +8,6 @@ sudo apt-get install esl-erlang
 # Elixir
 sudo apt-get install elixir
 
-# # nodejs (when using brunch)
-# sudo apt-get install curl
-# curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
-# sudo apt-get install -y nodejs
-
 # postgresql
 sudo apt-get install postgresql-client
 
